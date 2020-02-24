b43ae341f31254e21aee899537614400cb14bf3f
marvin evins <maevins@cps.edu>
2020-02-23 22:46:06 -0600

    working with kivy

---
+++ main.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text = "marvin gui")
+
+
+
+if __name__ == "__main__":
+    MyApp().run()
