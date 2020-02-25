2508a39261cb4a2f1ec8db90153c5951e3c99386
marvin evins <maevins@cps.edu>
2020-02-24 18:48:30 -0600

    working on form

--- main.py
+++ main.py
@@ -8,23 +8,42 @@ from kivy.uix.button import Button
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols = 2
+        self.cols = 1
 
-        self.add_widget(Label(text="First name: "))
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+
+
+        self.inside.add_widget(Label(text="First name: "))
         self.firstName = TextInput(multiline=False)
-        self.add_widget(self.firstName)
+        self.inside.add_widget(self.firstName)
 
-        self.add_widget(Label(text="Last name: "))
+        self.inside.add_widget(Label(text="Last name: "))
         self.lastName = TextInput(multiline=False)
-        self.add_widget(self.lastName)
+        self.inside.add_widget(self.lastName)
 
 
-        self.add_widget(Label(text="Email: "))
+        self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
 
         self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
         self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        name = self.firstName.text
+        last = self.lastName.text
+        email = self.email.text
+
+        print("Name: ", name, "Last Name: ", last, "Email ", email)
+        self.firstName.text = ""
+        self.lastName.text = ""
+        self.email.text = ""
+
 
 
 class MyApp(App):
